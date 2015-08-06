a05152e4a49f748d8a4509b8605251bf64c0c67f
Arun George <arung@git.dishonline.com>
2015-08-06 12:23:35 +0530

    updated testcases

--- src/test/java/pns/alltypes/rabbitmq/RabbitMQPublisherTest.java
+++ src/test/java/pns/alltypes/rabbitmq/RabbitMQPublisherTest.java
@@ -5,12 +5,10 @@ package pns.alltypes.rabbitmq;
 
 import java.io.IOException;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import pns.alltypes.rabbitmq.config.RabbitConnectionConfig;
-import pns.alltypes.rabbitmq.io.AmqpChannel;
-import pns.alltypes.rabbitmq.sustained.RabbitMQConnectionManager;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Address;
@@ -19,44 +17,50 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
+import pns.alltypes.rabbitmq.config.RabbitConnectionConfig;
+import pns.alltypes.rabbitmq.io.AmqpChannel;
+import pns.alltypes.rabbitmq.sustained.RabbitMQConnectionManager;
+
 /**
  * @author arung
  */
 public class RabbitMQPublisherTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQPublisherTest.class);
-    private static final RabbitMQPublisherTest RABBIT_MQ_PUBLISHER_TEST = new RabbitMQPublisherTest();
     private static final ConnectionFactory CONN_FACTORY = new ConnectionFactory();
 
+    private static String userName;
+    private static String password;
+    private static String queueName;
+    private static String host;
+
+    @BeforeClass
+    public static void init() {
+        RabbitMQPublisherTest.userName = System.getProperty("userName");
+        RabbitMQPublisherTest.password = System.getProperty("password");
+        RabbitMQPublisherTest.queueName = System.getProperty("queueName");
+        RabbitMQPublisherTest.host = System.getProperty("host");
+
+    }
+
     private RabbitMQConnectionManager RABBIT_MQ_CONNECTION_MANAGER;
-
-    private RabbitMQPublisherTest() {
-
-    }
-
-    public static void main(final String[] args) throws IOException {
-        if (args.length < 4) {
-            RabbitMQPublisherTest.LOGGER.error(String.format("Reqiured parameters missing. Use java -jar <jar file> user pass queueName Host"));
-        }
-
-        RabbitMQPublisherTest.RABBIT_MQ_PUBLISHER_TEST.testRabbitMQ(args[0], args[1], args[2], args[3]);
-    }
 
     /**
      * @param password
      * @param userName
      * @throws IOException
      */
-    private void testRabbitMQ(final String userName, final String password, final String queueName, final String host) throws IOException {
+    @Test
+    public void testRabbitMQConsumption() throws IOException {
 
-        RabbitMQPublisherTest.CONN_FACTORY.setUsername(userName);
-        RabbitMQPublisherTest.CONN_FACTORY.setPassword(password);
-        RabbitMQPublisherTest.CONN_FACTORY.setHost(host);
+        RabbitMQPublisherTest.CONN_FACTORY.setUsername(RabbitMQPublisherTest.userName);
+        RabbitMQPublisherTest.CONN_FACTORY.setPassword(RabbitMQPublisherTest.password);
+        RabbitMQPublisherTest.CONN_FACTORY.setHost(RabbitMQPublisherTest.host);
         RabbitMQPublisherTest.CONN_FACTORY.setRequestedHeartbeat(25);
         RABBIT_MQ_CONNECTION_MANAGER = RabbitMQConnectionManager.getInstance(new RabbitConnectionConfig(RabbitMQPublisherTest.CONN_FACTORY, new Address[] {}));
         RABBIT_MQ_CONNECTION_MANAGER.hintResourceAddition();
         final AmqpChannel channel = RABBIT_MQ_CONNECTION_MANAGER.getChannel();
         final Channel ioChannel = channel.getChannel();
-        ioChannel.basicConsume(queueName, false, new DefaultConsumer(ioChannel) {
+        ioChannel.basicConsume(RabbitMQPublisherTest.queueName, false, new DefaultConsumer(ioChannel) {
             /* (non-Javadoc)
              * @see com.rabbitmq.client.DefaultConsumer#handleDelivery(java.lang.String, com.rabbitmq.client.Envelope, com.rabbitmq.client.AMQP.BasicProperties, byte[])
              */
