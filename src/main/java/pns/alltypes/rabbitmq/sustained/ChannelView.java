a6da863434b4a1b36c64f1746b977d4e913104ec
Enisha <enisha.eshwar@echostar.com>
2016-06-07 15:21:49 +0530

    Added method to get channel with timeout

--- src/main/java/pns/alltypes/rabbitmq/sustained/ChannelView.java
+++ src/main/java/pns/alltypes/rabbitmq/sustained/ChannelView.java
@@ -3,52 +3,68 @@
  */
 package pns.alltypes.rabbitmq.sustained;
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pns.alltypes.rabbitmq.io.AmqpChannel;
 
 public class ChannelView {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ChannelView.class);
-    private final RabbitMQConnectionManager rabbitMQConnectionManager;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChannelView.class);
+	private final RabbitMQConnectionManager rabbitMQConnectionManager;
 
-    /**
-     * @param rabbitMQConnectionManager
-     */
-    public ChannelView(final RabbitMQConnectionManager rabbitMQConnectionManager) {
-        this.rabbitMQConnectionManager = rabbitMQConnectionManager;
-    }
+	/**
+	 * @param rabbitMQConnectionManager
+	 */
+	public ChannelView(final RabbitMQConnectionManager rabbitMQConnectionManager) {
+		this.rabbitMQConnectionManager = rabbitMQConnectionManager;
+	}
 
-    public AmqpChannel getChannel() {
-        AmqpChannel channel = null;
-        try {
-            ChannelView.LOGGER.trace("ChannelView: getting channel");
-            channel = rabbitMQConnectionManager.getChannelList().takeFirst();
-        } catch (final InterruptedException e) {
-            Thread.currentThread().interrupt();
+	public AmqpChannel getChannel() {
+		AmqpChannel channel = null;
+		try {
+			ChannelView.LOGGER.trace("ChannelView: getting channel");
+			channel = rabbitMQConnectionManager.getChannelList().takeFirst();
+		} catch (final InterruptedException e) {
+			Thread.currentThread().interrupt();
 
-        }
+		}
 
-        return channel;
+		return channel;
 
-    }
+	}
 
-    public void releaseChannel(final AmqpChannel channel) {
-        try {
-            if (!channel.isMarkForDeletion()) {
-                rabbitMQConnectionManager.getChannelList().putFirst(channel);
-            }
-        } catch (final InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
+	public AmqpChannel getChannelWithTimeout() {
+		AmqpChannel channel = null;
+		try {
+			ChannelView.LOGGER.trace("ChannelView: getting channel with timeout");
+			channel = rabbitMQConnectionManager.getChannelList().pollFirst(30, TimeUnit.SECONDS);
+		} catch (final InterruptedException e) {
+			Thread.currentThread().interrupt();
 
-    public void removeChannel(final AmqpChannel channel) {
-        rabbitMQConnectionManager.getChannelList().remove(channel);
-    }
+		}
 
-    public void addChannel(final AmqpChannel amqpChannel) {
-        rabbitMQConnectionManager.getChannelList().add(amqpChannel);
-    }
+		return channel;
+
+	}
+
+	public void releaseChannel(final AmqpChannel channel) {
+		try {
+			if (!channel.isMarkForDeletion()) {
+				rabbitMQConnectionManager.getChannelList().putFirst(channel);
+			}
+		} catch (final InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	public void removeChannel(final AmqpChannel channel) {
+		rabbitMQConnectionManager.getChannelList().remove(channel);
+	}
+
+	public void addChannel(final AmqpChannel amqpChannel) {
+		rabbitMQConnectionManager.getChannelList().add(amqpChannel);
+	}
 
 }
